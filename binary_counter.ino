f376f8c98f08cc482d76a57e8ee1ba49db8601af
eduardonk9999 <thiago.novak7@gmail.com>
2023-06-01 15:48:49 -0300

    feat: config pinos

--- binary_counter.ino
+++ binary_counter.ino
@@ -1,9 +1,14 @@
+// Definir os pinos dos LEDs
+int ledPins[] = {2, 3, 4, 5};
+int numPins = sizeof(ledPins) / sizeof(ledPins[0]);
+
+// Variavel para armazenar o valor atual do contador
+int contador = 0;
+
 void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Configura os pinosdos LEDs como saida
+  for (int i = 0; i < numPins; i++) {
+    pinMode(ledPins[i], OUTPUT)
+    
+  }
 }
